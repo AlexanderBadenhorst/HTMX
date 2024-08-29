79bb87d06e18f5a301491cc1d4dbd8eb4c8a3d0a
AlexanderBadenhorst <alexander.baden15.ab@gmail.com>
2024-08-29 18:06:09 +0200

    fourth commit

--- Project 01/app.js
+++ Project 01/app.js
@@ -1,27 +1,85 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
-const methodOverride = require('method-override');
-const router = require('./routes/index'); // <--- This line imports the router object
-
 const app = express();
+const port = 3000;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(methodOverride('_method'));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/', router); // modified line
+app.use(express.json());
 
-app.use((req, res, next) => {
-  res.status(404).send('Not Found');
+const booksFile = path.join(__dirname, 'books.json');
+
+const getBooks = () => {
+  return JSON.parse(fs.readFileSync(booksFile, 'utf8'));
+};
+
+const getBookById = (id) => {
+  const books = getBooks();
+  return books.find((book) => book.id === id);
+};
+
+const addBook = (book) => {
+  const books = getBooks();
+  book.id = books.length + 1;
+  books.push(book);
+  fs.writeFileSync(booksFile, JSON.stringify(books, null, 2));
+};
+
+const updateBook = (id, book) => {
+  const books = getBooks();
+  const index = books.findIndex((b) => b.id === id);
+  if (index !== -1) {
+    books[index] = book;
+    fs.writeFileSync(booksFile, JSON.stringify(books, null, 2));
+  }
+};
+
+const deleteBook = (id) => {
+  const books = getBooks();
+  const index = books.findIndex((b) => b.id === id);
+  if (index !== -1) {
+    books.splice(index, 1);
+    fs.writeFileSync(booksFile, JSON.stringify(books, null, 2));
+  }
+};
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my book API!');
 });
 
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send('Internal Server Error');
+app.get('/books', (req, res) => {
+  res.json(getBooks());
 });
 
-const server = app.listen(3000, () => {
-  console.log(`Express is running on port ${server.address().port}`);
+app.get('/books/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const book = getBookById(id);
+  if (book) {
+    res.json(book);
+  } else {
+    res.status(404).json({ error: 'Book not found' });
+  }
+});
+
+app.post('/books', (req, res) => {
+  const book = req.body;
+  addBook(book);
+  res.json(book);
+});
+
+app.put('/books/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const book = req.body;
+  updateBook(id, book);
+  res.json(book);
+});
+
+app.delete('/books/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  deleteBook(id);
+  res.json({ message: 'Book deleted successfully' });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
